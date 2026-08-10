e88350d77aaf8eed97acf59b773dc73456d1ef23
holtkevin13-sudo <holt.kevin13@gmail.com>
2026-08-09 20:18:24 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Horologion service worker.
    Bump CACHE whenever you change any file below, or phones will
    keep serving the old copy. That is the one maintenance chore. */
-const CACHE = 'horologion-v3';
+const CACHE = 'horologion-v4';
 
 const ASSETS = [
   './',
