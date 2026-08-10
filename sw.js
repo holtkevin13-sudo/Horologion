31dc4549e68ed3cbc3b6ffeaa3b01b7d9c787722
holtkevin13-sudo <holt.kevin13@gmail.com>
2026-08-10 15:27:17 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Horologion service worker.
    Bump CACHE whenever you change any file below, or phones will
    keep serving the old copy. That is the one maintenance chore. */
-const CACHE = 'prayerbook-v16';
+const CACHE = 'prayerbook-v21';
 
 const ASSETS = [
   './',
